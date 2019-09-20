bbba190e87e7e253207bc0bd612493cd471a96ca
Toan Pham <toanpham@Toans-MBP.net.fpt>
2019-09-20 19:03:45 +0700

    Updates

---
+++ precache-manifest.9d54eaa47699ea40667f117fd74461eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e51c0a3927b6019ce5ecabc6ff9cb13f",
+    "url": "/phimmoi/index.html"
+  },
+  {
+    "revision": "53c1718bcf9dbc23b0c5",
+    "url": "/phimmoi/static/css/main.08f55c4b.chunk.css"
+  },
+  {
+    "revision": "a97c9dd8df4401dd3f20",
+    "url": "/phimmoi/static/js/2.faf971b0.chunk.js"
+  },
+  {
+    "revision": "53c1718bcf9dbc23b0c5",
+    "url": "/phimmoi/static/js/main.481bad69.chunk.js"
+  },
+  {
+    "revision": "b6e08b6c4e60760019a2",
+    "url": "/phimmoi/static/js/runtime-main.618b1e0f.js"
+  },
+  {
+    "revision": "08494abf4210d1ea8b163791db70773c",
+    "url": "/phimmoi/static/media/reactMovie_logo.08494abf.png"
+  }
+]);
